52f708573f8e1e9a840b48d3fedd257498ca90bb
SAX_94 <matteosacchi94@gmail.com>
2022-06-09 18:43:01 +0200

    added useful comments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 new Vue({
     el: "#app",
     data: {
+        /* Chiavi necessarie all'applicazione */
         message: "This is my first Vue message",
     },
+    methods: {
+        /* Funzioni da usare nell'app */
+    }
 });
